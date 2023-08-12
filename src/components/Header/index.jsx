4029f440b9fbdb58d08c7dee0a56d32549a29be9
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-12 08:19:59 -0300

    style(header): Estilização de header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,15 @@
+import styles from './style.module.scss';
 import '../../styles/index.scss';
 import Logo from '../../assets/Logo.svg';
 
 export const Header = () => {
 	return (
-		<>
-			<img src={Logo} alt='Logo' />
-		</>
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<div className={styles.imgContainer}>
+					<img src={Logo} alt='Logo' />
+				</div>
+			</div>
+		</header>
 	);
 };
