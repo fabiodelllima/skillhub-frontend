34b4cc447d9aadc49fd8a9a328fcff022738132f
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-12 07:12:37 -0300

    chore: Configurações iniciais

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/index.scss';
+import { LoginPage } from './components/pages/LoginPage';
+
+const App = () => {
+	return (
+		<>
+			<LoginPage />
+		</>
+	);
+};
+
+export default App;
