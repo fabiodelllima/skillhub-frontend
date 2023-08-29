0248cb615e19ff3f3e430a1b9a6d63d19d181c09
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-28 21:00:59 -0300

    fix: Imports e props

--- src/components/Dashboard/Header/index.jsx
+++ src/components/Dashboard/Header/index.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import '../../styles/index.scss';
+import '../../../styles/index.scss';
 import styles from './style.module.scss';
-import Logo from '../../assets/Logo.svg';
+import Logo from '../../../assets/Logo.svg';
 import { Link } from 'react-router-dom';
 
-export const Header = ({ buttonText, userLogout, isUserPage }) => {
+export const Header = ({ buttonText, userLogout }) => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.container}>
--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Dashboard } from '../../components/Dashboard';
-import { Header } from '../../components/Header';
+import { Header } from '../../components/Dashboard/Header';
 
 export const UserPage = ({ user, userLogout }) => {
 	return (
 		<>
-			<Header buttonText='Sair' userLogout={userLogout} maxWidth='800px' />
+			<Header buttonText='Sair' userLogout={userLogout} />
 			<main>
 				<Dashboard user={user} />
 			</main>
