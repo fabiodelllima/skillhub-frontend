83f0b94ac11a20d969d516875738609475a2aebb
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-28 21:01:26 -0300

    feat: Criação de Contexto

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from './providers/Context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>
 );
