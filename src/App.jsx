023fd45f888f0e71a25fb9553c73f06863d0b1e4
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-30 15:08:48 -0300

    feat: Implementação de carregamento de página

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './styles/index.scss';
 import { RoutesMain } from './routes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useContext } from 'react';
+import { UserContext } from './providers/UserContext';
+import { Loading } from './components/Loading';
 
 const App = () => {
+	const { loading } = useContext(UserContext);
 	return (
 		<>
-			<RoutesMain />
+			{loading ? <Loading /> : <RoutesMain />}
 			<ToastContainer
 				position='bottom-left'
 				autoClose={2 * 1000}
