ea7c4177e56cde0ba13847ff33db53f5787aaf05
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-30 15:10:42 -0300

    feat: Criação de rotas privadas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,16 +3,19 @@ import { LoginPage } from '../pages/LoginPage';
 import { RegisterPage } from '../pages/RegisterPage';
 import { UserPage } from '../pages/UserPage';
 import { ErrorPage } from '../pages/ErrorPage';
+import { PrivateRoutes } from './PrivateRoutes';
 
 export const RoutesMain = () => {
 	return (
-		<>
-			<Routes>
-				<Route path='/' element={<LoginPage />} />
-				<Route path='/register' element={<RegisterPage />} />
+		<Routes>
+			<Route path='/' element={<LoginPage />} />
+			<Route path='/register' element={<RegisterPage />} />
+
+			<Route element={<PrivateRoutes />}>
 				<Route path='/user' element={<UserPage />} />
-				<Route path='*' element={<ErrorPage />} />
-			</Routes>
-		</>
+			</Route>
+
+			<Route path='*' element={<ErrorPage />} />
+		</Routes>
 	);
 };
