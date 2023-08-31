759db958373c830d2a4220125c76a6ddeaa84e3d
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-31 01:03:38 -0300

    feat: Criação de componente TechModal

---
+++ src/components/Dashboard/TechSection/TechModal/index.jsx
@@ -0,0 +1,25 @@
+import { Input } from '../../../Forms/Input';
+import { Select } from '../../../Forms/Select';
+import styles from './style.module.scss';
+
+export const TechModal = ({ onClose }) => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Cadastrar tecnologia</h1>
+        <div onClick={onClose} className={styles.closeBtnContainer}>
+          <span>X</span>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <form onSubmit='' className={styles.formContainer}>
+          <Input label='Nome' className={styles.input} />
+          <Select label='Selecionar status' className={styles.input} />
+          <button type='submit' className={styles.buttonPrimary}>
+            Cadastrar Tecnologia
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
