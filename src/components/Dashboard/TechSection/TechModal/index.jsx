6c90ab0ad72ff7fca609472c44676e5d2351dba1
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-31 02:54:27 -0300

    feat: Create pt.1

--- src/components/Dashboard/TechSection/TechModal/index.jsx
+++ src/components/Dashboard/TechSection/TechModal/index.jsx
@@ -1,20 +1,48 @@
+import { useForm } from 'react-hook-form';
 import { Input } from '../../../Forms/Input';
 import { Select } from '../../../Forms/Select';
 import styles from './style.module.scss';
+import { useContext } from 'react';
+import { TechContext } from '../../../../providers/TechContext';
 
 export const TechModal = ({ onClose }) => {
+  const { register, handleSubmit } = useForm();
+  const { createTech } = useContext(TechContext);
+
+  const submit = (formData) => {
+    createTech(formData);
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <h1 className={styles.title}>Cadastrar tecnologia</h1>
-        <div onClick={onClose} className={styles.closeBtnContainer}>
+        <div
+          onClick={onClose}
+          className={styles.closeBtnContainer}
+        >
           <span>X</span>
         </div>
       </header>
       <main className={styles.main}>
-        <form className={styles.formContainer}>
-          <Input label='Nome' className={styles.input} />
-          <Select label='Selecionar status' className={styles.input} />
+        <form
+          onSubmit={handleSubmit(submit)}
+          className={styles.formContainer}
+        >
+          <Input
+            label='Nome'
+            className={styles.input}
+            {...register('title')}
+          />
+          <Select
+            label='Selecionar status'
+            className={styles.input}
+            {...register('status')}
+          >
+            <option value='Iniciante'>Iniciante</option>
+            <option value='Intermediário'>Intermediário</option>
+            <option value='Avançado'>Avançado</option>
+          </Select>
           <button type='submit' className={styles.buttonPrimary}>
             Cadastrar Tecnologia
           </button>
--- src/providers/TechContext.jsx
+++ src/providers/TechContext.jsx
@@ -1,15 +1,28 @@
-import { createContext, useEffect, useState } from 'react';
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
 import { api } from '../services/api';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from './UserContext';
 
 export const TechContext = createContext({});
 
 export const TechProvider = ({ children }) => {
+  const { user } = useContext(UserContext);
+
   const [techList, setTechList] = useState([]);
 
+  const navigate = useNavigate();
+
+  // READ
   useEffect(() => {
     const getTechs = async () => {
       try {
-        const { data } = await api.post('/users/techs');
+        const { data } = await api.get('');
         console.table(data);
       } catch (error) {
         console.log(error);
@@ -18,7 +31,33 @@ export const TechProvider = ({ children }) => {
     };
   }, []);
 
+  // CREATE
+  const createTech = async (formData) => {
+    try {
+      const token = localStorage.getItem('@TOKEN');
+
+      const newTech = {
+        title: user.title,
+        status: user.status,
+        ...formData,
+      };
+
+      const { data } = await api.post('/users/techs', newTech, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.table(data);
+
+      navigate('/user');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <TechContext.Provider value={{ techList }}>{children}</TechContext.Provider>
+    <TechContext.Provider value={{ techList, createTech }}>
+      {children}
+    </TechContext.Provider>
   );
 };
