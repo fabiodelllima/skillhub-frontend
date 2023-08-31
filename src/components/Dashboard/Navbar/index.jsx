7229946c3c9d07bc22117a7e025b2ceb4937b565
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-30 22:19:49 -0300

    refactor: Header alterado para Navbar

---
+++ src/components/Dashboard/Navbar/index.jsx
@@ -0,0 +1,25 @@
+import '../../../styles/index.scss';
+import styles from './style.module.scss';
+import Logo from '../../../assets/Logo.svg';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../../../providers/UserContext';
+
+export const Navbar = ({ buttonText }) => {
+	const { userLogout } = useContext(UserContext);
+
+	return (
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<div className={styles.imgContainer}>
+					<img src={Logo} alt='Logo' />
+				</div>
+				<Link to='/'>
+					<button className={styles.buttonHeader} onClick={userLogout}>
+						{buttonText}
+					</button>
+				</Link>
+			</div>
+		</header>
+	);
+};
