b384bce00f3c33fd4fef82d1b5fa58995c0fc2b9
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-31 01:00:49 -0300

    chore: Ajuste em indentação

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,25 @@ import { UserContext } from './providers/UserContext';
 import { Loading } from './components/Loading';
 
 const App = () => {
-	const { loading } = useContext(UserContext);
+  const { loading } = useContext(UserContext);
 
-	return (
-		<>
-			{loading ? <Loading /> : <RoutesMain />}
-			<ToastContainer
-				position='bottom-left'
-				autoClose={2 * 1000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-				theme='dark'
-			/>
-		</>
-	);
+  return (
+    <>
+      {loading ? <Loading /> : <RoutesMain />}
+      <ToastContainer
+        position='bottom-left'
+        autoClose={2 * 1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='dark'
+      />
+    </>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './providers/UserContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<UserProvider>
-				<App />
-			</UserProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
