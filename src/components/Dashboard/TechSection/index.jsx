2d038870893416b9659ff67f115348635f367f43
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-31 01:02:53 -0300

    feat: Criação de componente TechSection

--- src/components/Dashboard/TechSection/index.jsx
+++ src/components/Dashboard/TechSection/index.jsx
@@ -1,28 +1,39 @@
+import { useState } from 'react';
 import { TechCard } from './TechCard';
 import styles from './style.module.scss';
+import { TechModal } from './TechModal';
 
 export const TechSection = () => {
-	return (
-		<section className={styles.container}>
-			<div className={styles.titleContainer}>
-				<h1 className={styles.title}>Tecnologias</h1>
-				<div className={styles.addTechBtnContainer}>
-					<span className={styles.title}>+</span>
-				</div>
-			</div>
-			<div className={styles.cardListContainer}>
-				<ul className={styles.cardList}>
-					<li>
-						<TechCard />
-					</li>
-					<li>
-						<TechCard />
-					</li>
-					<li>
-						<TechCard />
-					</li>
-				</ul>
-			</div>
-		</section>
-	);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleAddTechBtnClick = () => {
+    setIsVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsVisible(false);
+  };
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>Tecnologias</h1>
+        <div
+          onClick={handleAddTechBtnClick}
+          className={styles.addTechBtnContainer}
+        >
+          <span className={styles.title}>+</span>
+        </div>
+      </div>
+      <div className={styles.cardListContainer}>
+        <ul className={styles.cardList}>
+          <TechCard />
+          <TechCard />
+          <TechCard />
+        </ul>
+      </div>
+
+      {isVisible ? <TechModal onClose={handleCloseModal} /> : null}
+    </section>
+  );
 };
