88ce17aefb07f39321bcf0a82b07ec75310c1c83
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-31 01:03:20 -0300

    feat: Criação de componente TechCard

--- src/components/Dashboard/TechSection/TechCard/index.jsx
+++ src/components/Dashboard/TechSection/TechCard/index.jsx
@@ -1,16 +1,16 @@
 import styles from './style.module.scss';
 
 export const TechCard = () => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.titleContainer}>
-				<p className={styles.title}>React</p>
-			</div>
-			<div className={styles.detailsContainer}>
-				<p>Avançado</p>
-				<button>Editar</button>
-				<button>Excluir</button>
-			</div>
-		</div>
-	);
+  return (
+    <li className={styles.container}>
+      <div className={styles.titleContainer}>
+        <p className={styles.title}>React</p>
+      </div>
+      <div className={styles.detailsContainer}>
+        <p>Avançado</p>
+        <button>Editar</button>
+        <button>Excluir</button>
+      </div>
+    </li>
+  );
 };
