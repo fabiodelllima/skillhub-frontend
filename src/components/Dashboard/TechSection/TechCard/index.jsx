78d4872c38ff592592a4e75219896e50f400f7c6
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-31 21:37:17 -0300

    style: Redefinição de divs para finalizar estilização

--- src/components/Dashboard/TechSection/TechCard/index.jsx
+++ src/components/Dashboard/TechSection/TechCard/index.jsx
@@ -1,5 +1,6 @@
 import styles from './style.module.scss';
-import { MdEdit, MdDelete } from 'react-icons/md';
+import EditIcon from '../../../../assets/EditIcon.svg';
+import DeleteIcon from '../../../../assets/DeleteIcon.svg';
 import { useContext } from 'react';
 
 import { TechContext } from '../../../../providers/TechContext';
@@ -13,21 +14,33 @@ export const TechCard = ({ tech }) => {
         <p className={styles.title}>{tech.title}</p>
       </div>
       <div className={styles.detailsContainer}>
-        <p>{tech.status}</p>
-        <button
-          onClick={() => setEditTech(tech)}
-          title='Editar tecnologia'
-          aria-label='edit'
-        >
-          <MdEdit />
-        </button>
-        <button
-          onClick={() => deleteTech(tech.id)}
-          title='Excluir tecnologia'
-          aria-label='delete'
-        >
-          <MdDelete />
-        </button>
+        <p className={styles.status}>{tech.status}</p>
+        <div className={styles.iconsContainer}>
+          <button
+            className={styles.editButton}
+            title='Editar tecnologia'
+            aria-label='edit'
+            onClick={() => setEditTech(tech)}
+          >
+            <img
+              src={EditIcon}
+              alt='Editar tecnologia'
+              className={styles.icon}
+            />
+          </button>
+          <button
+            className={styles.deleteButton}
+            title='Excluir tecnologia'
+            aria-label='delete'
+            onClick={() => deleteTech(tech.id)}
+          >
+            <img
+              src={DeleteIcon}
+              alt='Excluir tecnologia'
+              className={styles.icon}
+            />
+          </button>
+        </div>
       </div>
     </li>
   );
