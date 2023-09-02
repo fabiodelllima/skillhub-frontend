d9ad74fbe0af5c2b5500a7e59d8e65bd3f43f602
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-09-01 23:58:50 -0300

    refactor: Alteração em lógica de ternário

--- src/routes/PublicRoutes/index.jsx
+++ src/routes/PublicRoutes/index.jsx
@@ -5,5 +5,5 @@ import { Navigate, Outlet } from 'react-router-dom';
 export const PublicRoutes = () => {
   const { user } = useContext(UserContext);
 
-  return !user ? <Outlet /> : <Navigate to={'/user'} />;
+  return user ? <Navigate to={'/user'} /> : <Outlet />;
 };
