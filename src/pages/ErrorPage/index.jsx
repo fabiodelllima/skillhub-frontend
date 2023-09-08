a217e67bdf85d1cafe7565fc6a74a0d72b3033b5
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-09-08 17:52:29 -0300

    refactor: Mensagens de erro em variáveis

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,13 +1,14 @@
 import styles from './style.module.scss';
 
 export const ErrorPage = () => {
+  const primaryMsg = 'Erro: 404';
+  const secondaryMsg = 'Não foi possível encontrar a página.';
+
   return (
     <section className={styles.container}>
       <div className={styles.subContainer}>
-        <h1 className={styles.title}>Erro: 404</h1>
-        <p className={styles.subTitle}>
-          Não foi possível encontrar a página.
-        </p>
+        <h1 className={styles.title}>{primaryMsg}</h1>
+        <p className={styles.subTitle}>{secondaryMsg}</p>
       </div>
     </section>
   );
